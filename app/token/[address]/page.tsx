87db969b259d690a0c0290bd6f43f982e01a2635
Lee Knowlton <leeknowlton@gmail.com>
2025-02-14 23:28:07 +0900

    Remove custom page title

--- app/token/[address]/page.tsx
+++ app/token/[address]/page.tsx
@@ -1,23 +1,4 @@
 import { TokenPageContent } from "./TokenPageContent";
-import { Metadata } from "next";
-import { getTokenData } from "@/app/lib/tokens"; // You'll need to implement this function
-
-type Props = {
-  params: {
-    address: string;
-  };
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  // Fetch token data - implement getTokenData to fetch token info from your data source
-  const tokenData = await getTokenData(params.address);
-
-  return {
-    title: `${tokenData.name} (${tokenData.symbol}) - Streme Fun`,
-    // You can also add other metadata like description if needed
-    description: `Stake and earn rewards for ${tokenData.name} token on Streme Fun`,
-  };
-}
 
 export default function TokenPage() {
   return <TokenPageContent />;
