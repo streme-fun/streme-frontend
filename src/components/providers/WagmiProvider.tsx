4581c36f898fa7306e06d98d393a690fb6c77b6b
Lee Knowlton <leeknowlton@gmail.com>
2025-05-27 16:43:00 +0900

    Re-add wallet changes

--- src/components/providers/WagmiProvider.tsx
+++ src/components/providers/WagmiProvider.tsx
@@ -42,9 +42,9 @@ function useCoinbaseWalletAutoConnect() {
 
   useEffect(() => {
     // Auto-connect if in Coinbase Wallet and not already connected
-    // if (isCoinbaseWallet && !isConnected) {
-    //   connect({ connector: connectors[1] }); // Coinbase Wallet connector
-    // }
+    if (isCoinbaseWallet && !isConnected) {
+      connect({ connector: connectors[1] }); // Coinbase Wallet connector
+    }
   }, [isCoinbaseWallet, isConnected, connect, connectors]);
 
   return isCoinbaseWallet;
