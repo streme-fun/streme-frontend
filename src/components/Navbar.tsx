6aa266d1b50a224cc6089279d6039742eff32506
Lee Knowlton <leeknowlton@gmail.com>
2025-05-30 20:30:48 +0900

    Add secret auth debug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -194,28 +194,6 @@ Symbol: $[your ticker]
               Launch
             </button>
 
-            {/* Auth Test Button */}
-            <Link
-              href="/auth-test"
-              className="flex flex-col items-center justify-center text-xs sm:text-sm text-gray-700 hover:text-primary flex-1"
-            >
-              <svg
-                className="w-6 h-6 mb-0.5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.031 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-                ></path>
-              </svg>
-              Auth Test
-            </Link>
-
             {/* Leaderboard Button */}
             <button
               onClick={() => {
