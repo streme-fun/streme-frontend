e1642b3c5785a38e2f28581c1539cbb90d402c0e
Lee Knowlton <leeknowlton@gmail.com>
2025-06-03 17:00:06 +0900

    Add posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ClientLayout from "./ClientLayout";
 import "./globals.css";
+import { PostHogProvider } from "../components/providers/PostHogProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased pb-4`}
       >
-        <ClientLayout>{children}</ClientLayout>
+        <PostHogProvider>
+          <ClientLayout>{children}</ClientLayout>
+        </PostHogProvider>
       </body>
     </html>
   );
