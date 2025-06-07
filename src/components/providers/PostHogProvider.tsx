ebe4f0c6f1af228f6cfbc9db351c2a6120c6b0df
Lee Knowlton <leeknowlton@gmail.com>
2025-06-07 23:31:07 +0900

    Turn off posthog session recording

--- src/components/providers/PostHogProvider.tsx
+++ src/components/providers/PostHogProvider.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import posthog from "posthog-js"
-import { PostHogProvider as PHProvider, usePostHog } from "posthog-js/react"
-import { Suspense, useEffect } from "react"
-import { usePathname, useSearchParams } from "next/navigation"
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider, usePostHog } from "posthog-js/react";
+import { Suspense, useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
@@ -11,37 +11,38 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       api_host: "/ingest",
       ui_host: "https://us.posthog.com",
       capture_pageview: false, // We capture pageviews manually
-      capture_pageleave: true,  // Enable pageleave capture
+      capture_pageleave: true, // Enable pageleave capture
       capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+      disable_session_recording: true, // Disable session recording
       debug: process.env.NODE_ENV === "development",
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <PHProvider client={posthog}>
       <SuspendedPostHogPageView />
       {children}
     </PHProvider>
-  )
+  );
 }
 
 function PostHogPageView() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const posthogClient = usePostHog()
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthogClient = usePostHog();
 
   useEffect(() => {
     if (pathname && posthogClient) {
-      let url = window.origin + pathname
-      const search = searchParams.toString()
+      let url = window.origin + pathname;
+      const search = searchParams.toString();
       if (search) {
-        url += `?${search}`
+        url += `?${search}`;
       }
-      posthogClient.capture("$pageview", { "$current_url": url })
+      posthogClient.capture("$pageview", { $current_url: url });
     }
-  }, [pathname, searchParams, posthogClient])
+  }, [pathname, searchParams, posthogClient]);
 
-  return null
+  return null;
 }
 
 function SuspendedPostHogPageView() {
@@ -49,5 +50,5 @@ function SuspendedPostHogPageView() {
     <Suspense fallback={null}>
       <PostHogPageView />
     </Suspense>
-  )
+  );
 }
