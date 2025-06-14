b66b0b4581da66b1dd83cd2b473eb03c67829fe5
Lee Knowlton <leeknowlton@gmail.com>
2025-06-14 15:56:05 +0900

    Clean up unused code

--- src/components/WalletSwitcher.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export function WalletSwitcher() {
-  // Hide the wallet switcher since the user only wants automatic switching
-  // based on browser extension changes, not manual switching between multiple wallets
-  return null;
-}
