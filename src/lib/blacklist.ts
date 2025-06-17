bb534a94e383f7ac5080f29b0c3a49da720869c6
Lee Knowlton <leeknowlton@gmail.com>
2025-06-18 00:03:49 +0900

    Update blacklist

--- src/lib/blacklist.ts
+++ src/lib/blacklist.ts
@@ -1,12 +1,8 @@
 export const SPAMMER_BLACKLIST = [
-  "nexisdao.eth", 
+  "nexisdao.eth",
   "caitlynjenner",
-  "nexisdao", 
-  "nexis",
-  "caitlyn.jenner",
-  "caitlyn",
-  // Add more variations and common spam patterns
-  "nexis.dao",
-  "nexisdao.base",
-  "nexisdaoeth"
+  "stockastick",
+  "zealous",
+  "revoxrexx",
+  "ismancuann",
 ];
